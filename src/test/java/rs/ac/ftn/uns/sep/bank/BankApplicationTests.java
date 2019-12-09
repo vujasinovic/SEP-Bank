c883efbe486666d33dc5c4996d8733f52e7729f6
Aleksandar Vujasinovic <aleksandar.vujasinov@gmail.com>
2019-12-09 19:25:12 +0100

    Initial commit

---
+++ src/test/java/rs/ac/ftn/uns/sep/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.ftn.uns.sep.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
